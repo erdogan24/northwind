49ee44b7722de78e2c7ff41997ddeed77049dcd5
erdoganakpolat <erdoganakpolat98@gmail.com>
2024-11-22 16:48:23 +0300

    commit 4

---
+++ northwind/src/main/java/projectJava/northwind/core/utilities/results/ErrorResult.java
@@ -0,0 +1,16 @@
+package projectJava.northwind.core.utilities.results;
+
+public class ErrorResult extends Result{
+	
+		public ErrorResult() {
+			super(false);
+			
+		}
+		
+		public ErrorResult(String message) {
+			
+			super(false,message);
+		}
+	}
+
+
