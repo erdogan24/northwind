af23bb37196927179cedf41cb3fbf8fd3a73e102
erdoganakpolat <erdoganakpolat98@gmail.com>
2024-11-22 12:02:36 +0300

    commit 2

--- northwind/src/main/java/projectJava/northwind/business/concretes/ProductManager.java
+++ northwind/src/main/java/projectJava/northwind/business/concretes/ProductManager.java
@@ -2,15 +2,29 @@ package projectJava.northwind.business.concretes;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import projectJava.northwind.business.abstracts.ProductService;
+import projectJava.northwind.dataAccess.abstracts.ProductDao;
 import projectJava.northwind.entities.concretes.Product;
 
+
+@Service
 public class ProductManager implements ProductService{
+	
+	private ProductDao productDao;
+	
+	@Autowired
+	public ProductManager(ProductDao productDao) {
+		super();
+		this.productDao = productDao;
+	}
 
 	@Override
 	public List<Product> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return this.productDao.findAll();
 	}
 
 }
