64e60c6ed6360fb3a1ae669e881a224ab8feebf3
erdoganakpolat <erdoganakpolat98@gmail.com>
2024-11-25 15:14:07 +0300

    commit 5

--- northwind/src/main/java/projectJava/northwind/business/abstracts/ProductService.java
+++ northwind/src/main/java/projectJava/northwind/business/abstracts/ProductService.java
@@ -2,6 +2,8 @@ package projectJava.northwind.business.abstracts;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+
 import projectJava.northwind.core.utilities.results.DataResult;
 import projectJava.northwind.core.utilities.results.Result;
 import projectJava.northwind.entities.concretes.Product;
@@ -9,4 +11,20 @@ import projectJava.northwind.entities.concretes.Product;
 public interface ProductService {
  	DataResult<List<Product>> getAll();
  	Result add(Product product);
+ 	
+ 	
+
+ 	DataResult<Product> getByProductName(String productName);
+	
+ 	DataResult<Product> getByProductNameAndCategoryId(String productName, int categoryId);
+	
+ 	DataResult<List<Product>> getByProductNameOrCategoryId(String productName, int categoryId);
+	
+ 	DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories);
+
+ 	DataResult<List<Product>> getByProductNameContains(String productName);	
+	
+ 	DataResult<List<Product>> getByProductNameStartsWith(String productName);
+	
+ 	DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId);
 }
